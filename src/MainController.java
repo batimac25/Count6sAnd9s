cd316589ffa5e47990143e6dd2ae4b9ad0b5bc2d
Antony Tibursias Infant <batimac25@gmail.com>
2017-07-20 15:13:20 +0530

    Did some modifications in con9s and 6s method. Still there bugs are there

--- src/MainController.java
+++ src/MainController.java
@@ -1,15 +1,48 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainController {
-	public static void main(String args[])
-	{
-		RandomNumbergenerator rn = new RandomNumbergenerator();
-		List<Integer> collection = new ArrayList<>();
-		rn.generateCollection(collection);
+	public static void main(String args[]) {
+		MainController m = new MainController();
 		Count6sAnd9s c = new Count6sAnd9s();
-		System.out.println(c.NumOf6sAnd9s(c.calculateNumOf6s(collection), c.calculateNumOf9s(collection)));
-		System.out.println(c.NumOfCon6sAnd9s(c.con6s(collection), c.con9s(collection)));
+
+		/*
+		 * RandomNumbergenerator rn = new RandomNumbergenerator(); List<Integer>
+		 * collection = new ArrayList<>(); long startTime =
+		 * System.currentTimeMillis(); rn.generateCollection(collection);
+		 * System.out.println(c.NumOf6sAnd9s(c.calculateNumOf6s(collection),
+		 * c.calculateNumOf9s(collection)));
+		 * System.out.println(c.NumOfCon6sAnd9s(c.con6s(collection),
+		 * c.con9s(collection))); long endTime = System.currentTimeMillis();
+		 * long totalTime = endTime - startTime;
+		 * System.out.println("The TotalRunTime is " + totalTime + " ms");
+		 */
+
+		// Test Cases
+
+		List<Integer> test1 = Arrays.asList(3, 6, 9, 9, 6, 6, 6, 9, 6, 9, 7, 66, 2, 6, 4, 3, 6, 6, 6, 4, 3, 5, 8, 9, 9,
+				99, 4, 6, 6, 5, 9, 9, 2, 3);
+		// List<Integer> test2 = Arrays.asList(7, 3, 9, 8, 9, 2, 6, 6, 9, 9, 7,
+		// 1, 9, 3, 6, 99, 9, 9, 9, 4, 6, 6, 9, 6, 9,
+		// 4, 5, 3, 9, 6, 3, 9, 3);
+		m.exampleTestCase(c.calculateNumOf6s(test1), 11, "No. of 6's");
+		m.exampleTestCase(c.calculateNumOf9s(test1), 8, "No. of 9's");
+		// m.exampleTestCase(c.calculateNumOf6s(test2), 7, "No. of 6's");
+		// m.exampleTestCase(c.calculateNumOf9s(test2), 12, "No. of 9's");
+		m.exampleTestCase(c.con6s(test1), 8, "No. of Consecutive 6's");
+		m.exampleTestCase(c.con9s(test1), 6, "No. of Consecutive 9's");
+		// m.exampleTestCase(c.con6s(test2), 4, "No. of Consecutive 6's");
+		// m.exampleTestCase(c.con9s(test2), 5, "No. of Consecutive 9's");
+
+	}
+
+	public void exampleTestCase(int a, int b, String whichTest) {
+		if (a == b) {
+
+			System.out.println("Test Pass for " + whichTest);
+		} else {
+			System.out.println("Test Fail for " + whichTest);
+		}
 	}
 
 }
--- src/RandomNumbergenerator.java
+++ src/RandomNumbergenerator.java
@@ -4,53 +4,43 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomNumbergenerator {
 	private static final int SIZE_OF_COLLECTION = 10000000;
-	private static final int MIN =1;
-	private static final int MAX =100;
-	
-	public void generateCollection(List<Integer> collection )
-	{   
+	private static final int MIN = 1;
+	private static final int MAX = 100;
+
+	public void generateCollection(List<Integer> collection) {
 		int value = 0;
-		for(int i = 0;i<SIZE_OF_COLLECTION;i++ )
-		{
-		value = ThreadLocalRandom.current().nextInt(1,MAX +1);
-		collection.add(value);
+		for (int i = 0; i < SIZE_OF_COLLECTION; i++) {
+			value = ThreadLocalRandom.current().nextInt(1, MAX + 1);
+			collection.add(value);
 		}
-		
+
 	}
-	public static void main(String args[])
-	{
+
+	public static void main(String args[]) {
 		int size = 0;
 		RandomNumbergenerator generator = new RandomNumbergenerator();
 		List<Integer> collection = new ArrayList<Integer>();
 		generator.generateCollection(collection);
-		if(collection.size() == SIZE_OF_COLLECTION)
-		{
+		if (collection.size() == SIZE_OF_COLLECTION) {
 			System.out.println("size test PASS");
-		}
-		else 
-		{
+		} else {
 			System.out.println("Fail");
 		}
-		
+
 		//
-		int count =0;
-		for(int i = 0;i<collection.size();i++)
-		{
-			
-			if(collection.get(i)>0&&collection.get(i)<100)
-			{
+		int count = 0;
+		for (int i = 0; i < collection.size(); i++) {
+
+			if (collection.get(i) > 0 && collection.get(i) < 100) {
 				count++;
-				
-			}
-			else 
-			{
-				System.out.println("Test Range Fails!!!"+ collection.get(i));
+
+			} else {
+				System.out.println("Test Range Fails!!!" + collection.get(i));
 				break;
 			}
-			System.out.println("Unreachable in some cases!!");	
+			System.out.println("Unreachable in some cases!!");
 		}
-		if(count == SIZE_OF_COLLECTION)
-		{
+		if (count == SIZE_OF_COLLECTION) {
 			System.out.println("Test range Pass");
 		}
 	}
